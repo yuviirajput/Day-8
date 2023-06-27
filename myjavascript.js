ec4fe713eecaba301a897e9178c3f1e7b3f71e8e
yuviirajput <137167194+yuviirajput@users.noreply.github.com>
2023-06-27 13:31:26 +0530

    Add files via upload

---
+++ myjavascript.js
@@ -0,0 +1,6 @@
+// javascript fill : my first script .js
+function StringLength(){
+    // retrieving the values of from element
+    var name = document.getElementById("name").value;
+    document.getElementById("length").innerText = name.length
+}
